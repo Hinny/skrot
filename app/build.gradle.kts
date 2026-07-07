5b0e5261ff3423d51bbf3ac1e47df7d236afa225
Hinny <henning.roos@gmail.com>
2026-07-07 13:17:39 +0200

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.hinny.skrot"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     // Release signing is provided via environment variables in CI (see README).
